b83984b49822bb65b324dc9c98fc2bcf26ff1bd8
Dmitry Marchuk <code@dmarchuk.com>
2023-03-08 19:58:28 +0200

    Git hooks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
 rootProject.name = "lunchmoney"
+
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.5"
+}
+
+gitHooks {
+    preCommit {
+        tasks("ktlintFormat", "ktlintCheck")
+    }
+    hook("prePush") {
+        tasks("clean", "build")
+    }
+    createHooks(true)
+}
