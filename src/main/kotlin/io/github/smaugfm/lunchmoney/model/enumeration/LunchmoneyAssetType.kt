33d196a32aeba641a21a43b0772df8d593211aa2
Dmitry Marchuk <code@dmarchuk.com>
2023-03-21 09:07:08 +0200

    Fix AssetType & enum serializer

--- src/main/kotlin/io/github/smaugfm/lunchmoney/model/enumeration/LunchmoneyAssetType.kt
+++ src/main/kotlin/io/github/smaugfm/lunchmoney/model/enumeration/LunchmoneyAssetType.kt
@@ -1,13 +1,23 @@
 package io.github.smaugfm.lunchmoney.model.enumeration
 
+import kotlinx.serialization.SerialName
+
 enum class LunchmoneyAssetType {
+    @SerialName("employee compensation")
     EMPLOYEE_COMPENSATION,
     CASH,
     VEHICLE,
     LOAN,
     CRYPTOCURRENCY,
     INVESTMENT,
-    OTHER,
     CREDIT,
-    REAL_ESTATE
+
+    @SerialName("real estate")
+    REAL_ESTATE,
+
+    @SerialName("other asset")
+    OTHER_ASSET,
+
+    @SerialName("other liability")
+    OTHER_LIABILITY
 }
--- src/main/kotlin/io/github/smaugfm/lunchmoney/serializer/LowercaseEnumSerializer.kt
+++ src/main/kotlin/io/github/smaugfm/lunchmoney/serializer/LowercaseEnumSerializer.kt
@@ -1,36 +1,43 @@
 package io.github.smaugfm.lunchmoney.serializer
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 internal open class LowercaseEnumSerializer<T : Enum<T>>(
-    private val serialName: String,
+    private val descriptorName: String,
     private val values: Array<T>
 ) : KSerializer<T> {
-    override val descriptor = PrimitiveSerialDescriptor(serialName, PrimitiveKind.STRING)
-    private val valueStrings = values.map { it.toString() }
+    override val descriptor = PrimitiveSerialDescriptor(descriptorName, PrimitiveKind.STRING)
+
+    private val enumToStrings = values.associateWith {
+        it.getSerialNameAnnotation()?.value ?: it.toString().lowercase()
+    }
+
+    private val stringsToEnum = enumToStrings.entries.associate {
+        it.value to it.key
+    }
 
     final override fun serialize(encoder: Encoder, value: T) {
-        val index = values.indexOf(value)
-            .also {
-                check(it >= 0) {
-                    "$value is not a valid enum $serialName, values are $values"
-                }
-            }
-        encoder.encodeString(values[index].toString().lowercase())
+        val str = enumToStrings[value]
+        check(str != null) {
+            "'$value' is not a valid enum $descriptorName, values are $values"
+        }
+        encoder.encodeString(str)
     }
 
     final override fun deserialize(decoder: Decoder): T {
-        val value = decoder.decodeString().uppercase()
-        val index = valueStrings.indexOf(value)
-            .also {
-                check(it >= 0) {
-                    "$value is not a valid enum $serialName, values are $values"
-                }
-            }
-        return values[index]
+        val str = decoder.decodeString()
+        val enumValue = stringsToEnum[str]
+        check(enumValue != null) {
+            "'$str' is not a valid enum $descriptorName, values are $values"
+        }
+        return enumValue
     }
+
+    private fun Enum<T>.getSerialNameAnnotation(): SerialName? =
+        javaClass.getDeclaredField(name).getAnnotation(SerialName::class.java)
 }
